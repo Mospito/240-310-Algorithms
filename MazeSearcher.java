715d7e49521b860ccd0265bbcfd12d3d56fc46ed
Mospito <jaturonmoonjan@gmail.com>
2021-03-14 17:48:36 +0700

    commit MazeSearch method explore

--- MazeSearcher.java
+++ MazeSearcher.java
@@ -6,9 +6,11 @@
       java MazeSearcher maze1.txt    (maze2.txt)
 */
 
-
+//6135512026 Jaturon Moonjan
 import java.io.*;
 import java.util.*;
+
+
 
 
 public class MazeSearcher 
@@ -31,10 +33,48 @@ public class MazeSearcher
 
 
 
-  private boolean explore(Maze maze, int x, int y,
-                                   ArrayList<Coord> path) 
+  private boolean explore(Maze maze, int x, int y,ArrayList<Coord> path) 
   {
     // Students: put your code here
+    //6135512026 Jaturon Moonjan
+    
+    //If Exit stop now!!
+    if(maze.isExit(x, y))  
+    {
+      return true;
+    }
+
+    //If Wall Don't Pass
+    if(maze.isWall(x, y))
+    {
+      return false;
+    }
+    //If wasVisited Don't Go
+    if(maze.wasVisited(x, y))
+    {
+      return false;
+    }
+
+    
+    if(maze.isValidLoc(x, y))
+    {
+      //Set Visited Now!
+      maze.setVisited(x, y);
+      if(   explore(maze, x, y+1, path)  //Find Up
+          ||explore(maze, x+1, y, path)  //Find Right
+          ||explore(maze, x, y-1, path)  //Find Down
+          ||explore(maze, x-1, y, path)) //Find Left
+      {
+        path.add(new Coord(x, y));
+        return true;
+      }
+      else
+        return false;
+    }
+
+    
+
+
     return false;
   }  // end of explore()
 
